048d86f02a295713a9db56bad04339fbcad007a1
7S-Codere <dylan.e33@hotmail.fr>
2025-10-08 01:01:35 +0200

    add pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+   { path: 'home', component: Home },
+   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  { path: 'products', component: Products },
+  {
+    path: 'products',
+    loadComponent: () => import('./pages/products/products').then(m => m.Products)
+  },
+  { path: 'about', component: About },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then(m => m.About)
+  },
+
+];
